fb616b5d35ffa5a8db607588c0fb3ec5288cafcc
braedan40 <braedan40@boringity.com>
2025-02-21 17:07:27 +0700

    Update Upgrade.lua

--- TDS/TDSTools/Functions/Upgrade.lua
+++ TDS/TDSTools/Functions/Upgrade.lua
@@ -1,4 +1,9 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Workspace = game:GetService("Workspace")
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local PathfindingService = game:GetService("PathfindingService")
 local RemoteFunction = if not GameSpoof then ReplicatedStorage:WaitForChild("RemoteFunction") else SpoofEvent
 local RemoteEvent = if not GameSpoof then ReplicatedStorage:WaitForChild("RemoteEvent") else SpoofEvent
 --[[{
@@ -9,6 +14,23 @@ local RemoteEvent = if not GameSpoof then ReplicatedStorage:WaitForChild("Remote
     ["Minute"] = number,
     ["Second"] = number,
 }]]
+        function MovePlayerToPosition(Position)
+    local character = LocalPlayer.Character or LocalPlayer.CharacterAdded:Wait()
+    local humanoid = character:FindFirstChild("Humanoid")
+    if not humanoid or not character.PrimaryPart then
+        return
+    end
+    local path = PathfindingService:CreatePath()
+    path:ComputeAsync(character.PrimaryPart.Position, Position)
+    local waypoints = path:GetWaypoints()
+    for _, waypoint in ipairs(waypoints) do
+        humanoid:MoveTo(waypoint.Position)
+        humanoid.MoveToFinished:Wait()
+     if waypoint.Action == Enum.PathWaypointAction.Jump then
+        humanoid.Jump = true
+        end
+    end
+end
 return function(self, p1)
     local tableinfo = p1--ParametersPatch("Upgrade",...)
     local Tower = tableinfo["TowerIndex"]
@@ -17,6 +39,7 @@ return function(self, p1)
     if not CheckPlace() then
         return
     end
+    
     local CurrentCount = StratXLibrary.CurrentCount
     SetActionInfo("Upgrade","Total")
     task.spawn(function()
@@ -55,6 +78,7 @@ return function(self, p1)
             return
         end
         SetActionInfo("Upgrade")
+        MovePlayerToPosition(TowersContained[Tower].Instance.Position)
         ConsoleInfo("Upgraded Tower Index: "..Tower..", Type: \""..TowerType.."\", (Wave "..Wave..", Min: "..Min..", Sec: "..Sec..", InBetween: "..tostring(InWave)..") UpgradeCheck: "..tostring(UpgradeCheck)..", SkipCheck: "..tostring(SkipCheck))
     end)
 end
