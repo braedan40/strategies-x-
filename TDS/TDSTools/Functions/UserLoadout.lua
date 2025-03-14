2ebcd8ba0bf60f70035cb3f3f96819fda0a57ca6
braedan40 <braedan40@boringity.com>
2025-03-14 22:11:52 +0700

    Update UserLoadout.lua

--- TDS/TDSTools/Functions/UserLoadout.lua
+++ TDS/TDSTools/Functions/UserLoadout.lua
@@ -2,13 +2,12 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 return function(self, p1)
     local tableinfo = p1
-    local GameSetLoadoutName = tableinfo["GameSetLoadout"]
     if not CheckPlace() then
         return
     end
     task.spawn(function()
 game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("PlayerManager"):WaitForChild("RE:UserLoadout"):FireServer()
 
-        ConsoleInfo("Choosen  User Loadout: "..GameSetLoadoutName)
+        ConsoleInfo("Choosen User Loadout")
     end)
 end
